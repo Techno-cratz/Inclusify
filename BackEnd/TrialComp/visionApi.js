d7ebed1991f2d1414d1663c6df1a59267638b192
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-09-17 22:29:42 -0600

    Add different api calls to cloud vision

--- BackEnd/TrialComp/visionApi.js
+++ BackEnd/TrialComp/visionApi.js
@@ -6,10 +6,7 @@ const client = new vision.ImageAnnotatorClient({
     keyFilename: "./VisionApi.json"
 });
 
-/**
- * TODO(developer): Uncomment the following line before running the sample.
- */
-// const fileName = 'Local image file, e.g. /path/to/image.png';
+
 async function test() {
     const fileName = './Steve.jpg';
 // Performs label detection on the local file
@@ -19,4 +16,26 @@ async function test() {
     labels.forEach(label => console.log(label.description));
 }
 
-test();
+// test();
+
+async function detectText(fileName) {
+  const [result] = await client.textDetection(fileName);
+  const detections = result.textAnnotations;
+  console.log('Text:');
+  detections.forEach(text => console.log(text));
+  
+  // This contains the entire text from the photo
+  // detections[0].description
+}
+
+// detectText('./Steve.jpg');
+
+async function detectLandmarks(fileName) {
+  const [result] = await client.landmarkDetection(fileName);
+  const landmarks = result.landmarkAnnotations;
+  console.log('Landmarks:');
+  landmarks.forEach(landmark => console.log(landmark));
+}
+
+// detectLandmarks('/Users/mehar/Downloads/Eiffel.jpg')
+
