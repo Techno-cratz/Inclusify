e72713e4fbf1ca4302253a6c8795b071eed049f7
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-09-18 17:08:56 -0600

    Fix btn tag overlap

--- FrontEnd/TrialComp/inclusify/src/components/InputBox/InputBox.js
+++ FrontEnd/TrialComp/inclusify/src/components/InputBox/InputBox.js
@@ -17,21 +17,30 @@ const InputBox = () => {
       <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
       <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
       <div class="container">
-        <div class="row">
-          <div class="col-md-6" style={imageFile==null ? {} : {display: 'none'}}>
-          {/* style={imageFile==null ? {} : {display: 'none'}} */}
+        <div >
+          <div class="col-md-6" style={imageFile == null ? {} : { display: 'none' }}>
+            {/* style={imageFile==null ? {} : {display: 'none'}} */}
             <form method="post" action="#" id="#" >
               <div class="form-group files">
                 {/* <label>Upload Your File </label> */}
-                <input type="file"  id="myFile" name="filename" class="form-control" multiple="" onChange={addImage}/>
+                <input type="file" id="myFile" name="filename" class="form-control" multiple="" onChange={addImage} />
+              </div>
+              <div class="form-group">
+                {/* <label for="exampleFormControlTextarea1">Example textarea</label> */}
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary">Submit</button>
               </div>
             </form>
           </div>
-          <div class="col-md-6">
-            <img src={imageFile} class="form-control"/>
+          <div class="col-md-6 containerBox" style={imageFile != null ? {} : { display: 'none' }}>
+            <img src={imageFile} class="form-control" />
+            <button class="btnBox">Button</button>
           </div>
         </div>
       </div>
+
       {/* ------------------- */}
 
     </>
