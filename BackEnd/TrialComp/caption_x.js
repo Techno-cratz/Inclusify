60234abd33ccbce3b19ed3230b70371a68a48f43
Raquib Khan Lavani <r.raquib01@gmail.com>
2021-09-18 15:09:12 -0600

    HASHTAGS WORKING

--- BackEnd/TrialComp/caption_x.js
+++ BackEnd/TrialComp/caption_x.js
@@ -1,6 +1,7 @@
-const request = require('request');
+var axios = require("axios").default;
+const camelCase = require('camelcase');
 var temp = ""
-class Caption {
+class Caption {         // #ilovenature  ---> api call to languaetool --> i Love Nature --> #iLoveNature
     constructor(caption) {
         this.caption = caption;
     }
@@ -9,7 +10,7 @@ class Caption {
         // Will find the emojis > Make new string with no emoji > Make another string with emojis
     }
 
-    correcthashtags() {
+    async correcthashtags() {
         let tags =  this.caption.match(/#[a-z]+/gi);
         let temp = "abcd";
         let correct_tags = "";
@@ -20,25 +21,26 @@ class Caption {
             tags[i] = tags[i].replace(/\#/g, '');
         }
         for (let i=0; i<tags.length; i++) {
-            fetch("https://dnaber-languagetool.p.rapidapi.com/v2/check", {
-                "method": "POST",
-                "headers": {
-                    "content-type": "application/x-www-form-urlencoded",
-                    "x-rapidapi-host": "dnaber-languagetool.p.rapidapi.com",
-                    "x-rapidapi-key": "1569dfa3aemsha00b48996627551p1f11d6jsn7ad35603d960"
+            console.log(tags[i]);
+            var options = {
+                method: 'POST',
+                url: 'https://dnaber-languagetool.p.rapidapi.com/v2/check',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'x-rapidapi-host': 'dnaber-languagetool.p.rapidapi.com',
+                    'x-rapidapi-key': '1569dfa3aemsha00b48996627551p1f11d6jsn7ad35603d960'
                 },
-                "body": {
-                    "text": tags[i],
-                    "language": "en-US"
-                }
-            })
-            .then(response => {
-                console.log(response);
-            })
-            .catch(err => {
-                console.error(err);
-            });
+                params: {text: tags[i], language: 'en-US'}
+                };
+                
+                var str = await axios.request(options).then(function (response) {
+                    return response.data["matches"][0]["replacements"][0]["value"];
+                }).catch(function (error) {
+                    console.error(error);
+                });
+                tags[i] = hash.concat(camelCase(str));
         }
+        console.log(tags);
         return tags;
     }
 
@@ -51,5 +53,5 @@ class Caption {
     }
 }
 
-let cap = new Caption("My trip to Lake Louise. #beautifulscenery")
+let cap = new Caption("My trip to Lake Louise. #beautifulscenery #iloveyou #lovemonkeys ")
 console.log(cap.correcthashtags())
