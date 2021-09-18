aa805e4c4aae26c7bf5c419bcd0d05c9628320dd
Vardan Saini <Vardan1@ualberta.ca>
2021-09-17 21:55:43 -0600

    VisionTestComplete

--- BackEnd/TrialComp/visionApi.js
+++ BackEnd/TrialComp/visionApi.js
@@ -2,15 +2,21 @@
 const vision = require('@google-cloud/vision');
 
 // Creates a client
-const client = new vision.ImageAnnotatorClient();
+const client = new vision.ImageAnnotatorClient({
+    keyFilename: "./VisionApi.json"
+});
 
 /**
  * TODO(developer): Uncomment the following line before running the sample.
  */
 // const fileName = 'Local image file, e.g. /path/to/image.png';
-const fileName = '/Users/mehar/Downloads/images.jpeg';
+async function test() {
+    const fileName = '/Users/vardansaini/Pictures/BG/Steve.jpg';
 // Performs label detection on the local file
-const [result] = await client.labelDetection(fileName);
-const labels = result.labelAnnotations;
-console.log('Labels:');
-labels.forEach(label => console.log(label.description));
+    const [result] = await client.labelDetection(fileName);
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+}
+
+test();
